845267f036cc7a06f22e5c369e6f3886442e3449
Kunwar <saluja@adobe.com>
2022-11-14 13:17:43 -0500

    toggle poc

---
+++ blocks/toggleview/toggleview.js
@@ -0,0 +1,22 @@
+export default function decorate(block) {
+
+    var videoTutorial = document.getElementById('video-based-learning');
+    var TextTutorial = document.getElementById('instruction-based-learning');
+
+    var videoTutorialSection = document.getElementsByClassName("video-tutorial")[0];
+    var textTutorialSection = document.getElementsByClassName("instruction-tutorial")[0];
+
+    videoTutorial.addEventListener("click", function(){        
+        videoTutorialSection.style.display='block'; 
+        textTutorialSection.style.display='none'; 
+        
+    });
+
+    TextTutorial.addEventListener("click", function(){
+        videoTutorialSection.style.display='none'; 
+        textTutorialSection.style.display='block'; 
+
+    
+    });
+
+}
