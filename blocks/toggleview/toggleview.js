b419fcb3884692721727b0595766ee179e3b3288
vmitra <vmitra@adobe.com>
2022-11-15 14:30:27 -0500

    2_commit

--- blocks/toggleview/toggleview.js
+++ blocks/toggleview/toggleview.js
@@ -7,22 +7,24 @@ export default function decorate(block) {
     });
     block.textContent = '';
     block.append(ul);
-    var videoTutorial = document.getElementById('video-based-learning');
-    var TextTutorial = document.getElementById('instruction-based-learning');
-
-    var videoTutorialSection = document.getElementsByClassName("video-tutorial")[0];
-    var textTutorialSection = document.getElementsByClassName("instruction-tutorial")[0];
-
+    let videoTutorial = document.getElementById('video-based-learning');
+    let TextTutorial = document.getElementById('instruction-based-learning');
+    TextTutorial.style.backgroundColor = '#808080';
+    videoTutorial.style.backgroundColor = '#808080';
+    let videoTutorialSection = document.getElementsByClassName("video-tutorial")[0];
+    let textTutorialSection = document.getElementsByClassName("instruction-tutorial")[0];
     videoTutorial.addEventListener("click", function () {
         videoTutorialSection.style.display = 'block';
         textTutorialSection.style.display = 'none';
+        videoTutorial.style.backgroundColor = '#000000';
+        TextTutorial.style.backgroundColor = '#808080';
 
     });
-
     TextTutorial.addEventListener("click", function () {
         videoTutorialSection.style.display = 'none';
         textTutorialSection.style.display = 'block';
-
+        TextTutorial.style.backgroundColor = '#000000';
+        videoTutorial.style.backgroundColor = '#808080';
 
     });
 
